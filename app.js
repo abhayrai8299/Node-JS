0e10cf733fe3e438e7dc477ace0741733ba0961d
Abhay Rai <abhay.rai8299@gmail.com>
2023-09-02 12:52:37 +0530

    first-commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const name=require('./utils');
+// const fs=require('fs');
+// fs.writeFileSync('notes.txt','This file was create by Abhay Rai \n');
+// fs.appendFileSync('notes.txt','This file was create by Node');
+console.log(name)
+console.log(process.argv);
---
+++ utils.js
@@ -0,0 +1,5 @@
+console.log("Util");
+
+const name="Mike";
+
+module.exports=name;
